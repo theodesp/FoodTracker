a350350d4582e0e2264bdd93ac6ecfff54c34d9f
Theo <theo.despoudis@teckro.com>
2019-02-12 17:57:17 +0000

    Add TableViewController

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by IT Spare on 31/01/2019.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -7,10 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -29,7 +28,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
     }
     
     //MARK: UIImagePickerControllerDelegate
